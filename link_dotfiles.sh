7bcc0fa36feb06eeb6cbf3dbe0024ee008eb96a1
Aurélie Grebe <aureliegrebe@gmail.com>
2023-01-16 21:40:09 -0500

    Add alacritty and polybar configs

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -7,6 +7,8 @@ then
   ln -svf ~/dotfiles/olympus/.vimrc ~/.vimrc
   ln -svf ~/dotfiles/olympus/.inputrc ~/.inputrc
   ln -svf ~/dotfiles/olympus/i3-config ~/.config/i3/config
+  ln -svf ~/dotfiles/olympus/alacritty-config ~/.config/alacritty/config
+  ln -svf ~/dotfiles/olympus/polybar-config ~/.config/polybar/config
   ln -svf ~/dotfiles/link_dotfiles.sh ~/.scripts/link_dotfiles.sh
 fi
 
